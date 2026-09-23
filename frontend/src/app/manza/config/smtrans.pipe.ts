974bd44a986d448d240bb63c0ad9a81a7f085134
Jose Martinez <jmartinez@solarmanes.es>
2026-09-23 08:48:55 +0200

    cambios verticales

--- frontend/src/app/manza/config/smtrans.pipe.ts
+++ frontend/src/app/manza/config/smtrans.pipe.ts
@@ -11,10 +11,15 @@ export class SMTransPipe implements PipeTransform {
         if (value == "AL") nvalue = "AMBOS LADOS";
         if (value == "TEC") nvalue = "TECHO";
         if (value == "P06") nvalue = "PARED 6 cm";
-        if (value == "P08") nvalue = "PARED 9 cm";        
+        if (value == "P08") nvalue = "PARED 8 cm";        
+        if (value == "P10") nvalue = "PARED 10 cm";
         if (value == "P12") nvalue = "PARED 12 cm";
+        if (value == "P14") nvalue = "PARED 14 cm";
         if (value == "P15") nvalue = "PARED 15 cm";        
         if (value == "P17") nvalue = "PARED 17 cm";
+        if (value == "P19") nvalue = "PARED 19 cm";
+        if (value == "P21") nvalue = "PARED 21 cm";
+        if (value == "P23") nvalue = "PARED 23 cm";
         if (value == "089") nvalue = "89 mm";
         if (value == "127") nvalue = "127 mm";
         if (value == "COR") nvalue = "CORDON";
@@ -22,7 +27,7 @@ export class SMTransPipe implements PipeTransform {
         if (value == "MSM") nvalue = "MOTOR SIN MANDO";
         if (value == "MM1") nvalue = "MOTOR MANDO 1 CANAL"; 
         if (value == "MM6") nvalue = "MOTOR MANDO 6 CANALES";                                
-        return  nvalue;
+        return nvalue || value;
      }
 }
 
--- frontend/src/app/manza/vertical/vertical.component.ts
+++ frontend/src/app/manza/vertical/vertical.component.ts
@@ -406,11 +406,11 @@ export class VerticalComponent implements OnInit, OnChanges {
    private calcularTejidosCombinados(): void {
       if (this.PV_SEL_1) {
          const combinados = this.Tipo3_TejidoColor1 !== this.Tipo3_TejidoColor2;
-         this.precios.T3_TejidosCombinados = combinados ? 20.51 : 0;
+         this.precios.T3_TejidosCombinados = combinados ? 46.97 : 0;
          this.precios.T3_TejidosCombinados_C1 = combinados
-            ? this.getCombinados_C1(this.precios.T3_Cantidad, 1052)
+            ? this.getCombinados_C1(this.precios.T3_Cantidad, 2472)
             : '';
-         this.Tipo3_Incremento_Tejidos = combinados ? '+ 20.51€ (C1 0001052)' : '';
+         this.Tipo3_Incremento_Tejidos = combinados ? '+ 46.97€ (C1 0002472)' : '';
       }
 
       if (this.PV_SEL_2) {
@@ -435,6 +435,13 @@ export class VerticalComponent implements OnInit, OnChanges {
 
       if (soporte === '-1' || sancho === '0') {
          this.resetSoporte(usaSel1);
+         if (soporte === 'TEC') {
+            if (usaSel1) {
+               this.precios.T3_TipoSoporte_C1 = 'C1 0000000';
+            } else {
+               this.precios.T32_TipoSoporte_C1 = 'C1 0000000';
+            }
+         }
          this.CalculateC1_Tipo3();
          return;
       }
@@ -455,7 +462,16 @@ export class VerticalComponent implements OnInit, OnChanges {
    }
 
    private getSanchoFromSoporte(soporte: string): string {
-      const map: Record<string, string> = { P08: '80', P12: '120', P15: '150' };
+      const map: Record<string, string> = {
+         P08: '80',
+         P10: '100',
+         P12: '120',
+         P14: '140',
+         P15: '150',
+         P19: '190',
+         P21: '210',
+         P23: '230'
+      };
       return map[soporte] || '0';
    }
 
@@ -506,12 +522,32 @@ export class VerticalComponent implements OnInit, OnChanges {
             sancho = "80";
          }
 
+         if (soporte == "P10") {
+            sancho = "100";
+         }
+
          if (soporte == "P12") {
             sancho = "120";
          }
 
+         if (soporte == "P14") {
+            sancho = "140";
+         }
+
          if (soporte == "P15") {
             sancho = "150";
+         }
+
+         if (soporte == "P19") {
+            sancho = "190";
+         }
+
+         if (soporte == "P21") {
+            sancho = "210";
+         }
+
+         if (soporte == "P23") {
+            sancho = "230";
          }
 
          soportes = Number(ancho) / 50.0;
@@ -577,12 +613,32 @@ export class VerticalComponent implements OnInit, OnChanges {
             sancho = "80";
          }
 
+         if (soporte == "P10") {
+            sancho = "100";
+         }
+
          if (soporte == "P12") {
             sancho = "120";
          }
 
+         if (soporte == "P14") {
+            sancho = "140";
+         }
+
          if (soporte == "P15") {
             sancho = "150";
+         }
+
+         if (soporte == "P19") {
+            sancho = "190";
+         }
+
+         if (soporte == "P21") {
+            sancho = "210";
+         }
+
+         if (soporte == "P23") {
+            sancho = "230";
          }
 
          soportes = Number(ancho) / 50.0;
@@ -593,6 +649,11 @@ export class VerticalComponent implements OnInit, OnChanges {
 
          this.precios.T32_TipoSoporte = 0;
          this.precios.T32_NumSoportes = 0;
+
+         if (soporte == "TEC") {
+            this.precios.T32_TipoSoporte_C1 = 'C1 0000000';
+         }
+
          if (sancho != "0") {
 
             this.service.getTarifaAccesorio("1", sancho, soportes).subscribe(
@@ -608,6 +669,7 @@ export class VerticalComponent implements OnInit, OnChanges {
                      this.precios.T32_NumSoportes = 0;
                      this.precios.T32_TipoSoporte_C1 = '';
                   }
+                  this.CalculateC1_Tipo3();
                },
                error => {
 
@@ -658,15 +720,15 @@ export class VerticalComponent implements OnInit, OnChanges {
          var color2 = this.Tipo3_TejidoColor2;
 
          if (color1 != color2) {
-            this.Tipo3_Incremento_Tejidos = "+ 20.51€ (C1 0001052)"
-            this.precios.T3_TejidosCombinados = 20.51;
-            this.precios.T3_TejidosCombinados_C1 = "C1 0001052";
+            this.Tipo3_Incremento_Tejidos = "+ 46.97€ (C1 0002472)";
+            this.precios.T3_TejidosCombinados = 46.97;
+            this.precios.T3_TejidosCombinados_C1 = this.getCombinados_C1(cantidad, 2472);
 
-            if (cantidad == 2) this.precios.T3_TejidosCombinados_C1 = "C1 0002104";
-            if (cantidad == 3) this.precios.T3_TejidosCombinados_C1 = "C1 0003156";
-            if (cantidad == 4) this.precios.T3_TejidosCombinados_C1 = "C1 0004208";
-            if (cantidad == 5) this.precios.T3_TejidosCombinados_C1 = "C1 0005260";
-            if (cantidad == 6) this.precios.T3_TejidosCombinados_C1 = "C1 0006312";
+            if (cantidad == 2) this.precios.T3_TejidosCombinados_C1 = "C1 0004944";
+            if (cantidad == 3) this.precios.T3_TejidosCombinados_C1 = "C1 0007416";
+            if (cantidad == 4) this.precios.T3_TejidosCombinados_C1 = "C1 0009888";
+            if (cantidad == 5) this.precios.T3_TejidosCombinados_C1 = "C1 0012360";
+            if (cantidad == 6) this.precios.T3_TejidosCombinados_C1 = "C1 0014832";
 
          }
          else {
@@ -809,13 +871,22 @@ export class VerticalComponent implements OnInit, OnChanges {
       const tipo: CortinaTipo = new CortinaTipo(this.codeCentro);
 
       if (this.PV_SEL_1) {
+         const posTej = this.getPos(this.pv_tejidos_1, parseInt(this.Tipo3_Tejido));
+         const tejidoText = posTej >= 0 ? this.pv_tejidos_1[posTej].text : '';
+
+         const posC1 = this.getPos(this.pv_tejidosC_1, parseInt(this.Tipo3_TejidoColor1));
+         const tejidoC1Text = posC1 >= 0 ? this.pv_tejidosC_1[posC1].text : '';
+
+         const posC2 = this.getPos(this.pv_tejidosC_2, parseInt(this.Tipo3_TejidoColor2));
+         const tejidoC2Text = posC2 >= 0 ? this.pv_tejidosC_2[posC2].text : '';
+
          tipo.Vertical_Add_T1(
             this.PV_Ancho_1, this.PV_Alto_1, this.PV_Cantidad_1,
             this.PV_AnchoLama_1, this.PV_PosicionMecanismo_1, this.PV_ColorRiel_1,
             this.PV_Accionamiento_1, this.PV_TipoSoporte_1, this.PV_TipoRecogida_1,
-            parseInt(this.Tipo3_Tejido), '',
-            parseInt(this.Tipo3_TejidoColor1), '',
-            parseInt(this.Tipo3_TejidoColor2), '',
+            parseInt(this.Tipo3_Tejido), tejidoText,
+            parseInt(this.Tipo3_TejidoColor1), tejidoC1Text,
+            parseInt(this.Tipo3_TejidoColor2), tejidoC2Text,
             this.Tipo3_Estancia, this.Tipo3_Estancia_Obs,
             Number(this.TipoVertical),
             this.PV_ID_Quiero_1, this.PV_ID_Imagen_1,
